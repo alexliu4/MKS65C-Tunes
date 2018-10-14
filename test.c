818d5d3dd5d2ec243a04d8357906974a052f7d33
alexliu4 <aliu9@stuy.edu>
2018-10-14 19:45:49 -0400

    "tuning"

--- test.c
+++ test.c
@@ -18,7 +18,7 @@ int main(){
   printf("Finding Specfic: %p\n\n",(find(d, "amy", "santiago")) );
 
   //removes d
-  remove_node(d, d);
+  remove_node(d, d); 
   printf("=====Removing first element=====\n");
   print_list(c);
   printf("=====Finished Printing New List=====\n\n");
