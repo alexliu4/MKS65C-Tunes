c1c0e862e33d183b14fc3858cdb7db0fef2fb7c8
alexliu4 <aliu9@stuy.edu>
2018-10-15 15:14:50 -0400

    "changing the test"

--- test.c
+++ test.c
@@ -6,9 +6,9 @@
 int main(){
   srand(time(NULL));
   struct song_node * a = insert_front(NULL,"alex", "hi");
-  struct song_node * b = insert_front(a,"karen", "hello");
-  struct song_node * c = insert_front(b,"amy", "santiago");
-  struct song_node * d = insert_front(c,"jake", "peralta");
+  struct song_node * b = insert_front(a,"amy", "santiago");
+  struct song_node * c = insert_front(b,"jake", "peralta");
+  struct song_node * d = insert_front(c,"karen", "hello");
   printf("=====Printing list=====\n");
   print_list(d);
   printf("=====Finished printing list=====\n\n");
@@ -18,7 +18,7 @@ int main(){
   printf("Finding Specfic: %p\n\n",(find(d, "amy", "santiago")) );
 
   //removes d
-  remove_node(d, d); 
+  remove_node(d, d);
   printf("=====Removing first element=====\n");
   print_list(c);
   printf("=====Finished Printing New List=====\n\n");
